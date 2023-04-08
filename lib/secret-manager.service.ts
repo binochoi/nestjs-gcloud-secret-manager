2a14e3177eb46a94d568a80a98f9c3e07c7fef52
bino <bonoself@gmail.com>
2023-04-08 22:05:16 +0900

    feat: add a versioning of secret

--- lib/secret-manager.service.ts
+++ lib/secret-manager.service.ts
@@ -8,19 +8,18 @@ export class SecretManagerService {
         @Inject(CLIENT_INSTANCE) private readonly client: SecretManagerClient,
         @Inject(SECRETS_PARENT) private readonly parent: string
     ) {}
-    async getSecret(secretName: string) {
+    async getSecret(secretName: string, version: 'latest' | number = 'latest') {
         const secretCache = _secrets.get(secretName);
         if(secretCache) {
             return secretCache;
         }
-        const secretFullName = `projects/${this.parent}/secrets/${secretName}/versions/` + 'latest';
+        const secretFullName = `projects/${this.parent}/secrets/${secretName}/versions/${version}`;
         const [ secret ] = await this.client.accessSecretVersion({ name: secretFullName });
         
         const secretValue = secret.payload?.data?.toString();
-        if(secretValue === undefined) {
-            return secretValue;
+        if(secretValue) {
+            _secrets.set(secretName, secretValue);
         }
-        _secrets.set(secretName, secretValue);
         return secretValue;
     }
 }
