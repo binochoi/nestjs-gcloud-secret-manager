cd2816cb50f08fe314f1542e693b442eed2c376a
bino <bonoself@gmail.com>
2023-04-09 23:18:37 +0900

    style(types):

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,14 +3,13 @@ import type { ClientOptions } from 'google-gax/build/src/clientInterface';
 import { v1 as secretManager } from "@google-cloud/secret-manager";
 export type SecretManagerClient = secretManager.SecretManagerServiceClient;
 interface DefaultOptions {
-    /** parent of secrets */
-    parent: number;
+  /** parent of secrets */
+  parent: number;
 }
 export type ModuleOptions = DefaultOptions & ClientOptions;
 
-export type AsyncProvider<T> =
+export type ModuleAsyncOptions<T> =
   | Omit<ClassProvider<T>, 'provide'>
   | Omit<ValueProvider<T>, 'provide'>
   | Omit<FactoryProvider<T>, 'provide'>
   | Omit<ExistingProvider<T>, 'provide'>;
-export type ModuleAsyncOptions<T> = AsyncProvider<T>;
