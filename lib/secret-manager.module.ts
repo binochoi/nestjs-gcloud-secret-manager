760028ccf02ede3deef9cf50e1bc979de2b159b4
bino <bonoself@gmail.com>
2023-04-09 17:43:05 +0900

    feat: add forRootAsync

--- lib/secret-manager.module.ts
+++ lib/secret-manager.module.ts
@@ -1,29 +1,38 @@
+import { ModuleAsyncOptions } from './types/index';
 import { Global, Module, DynamicModule } from '@nestjs/common';
 import { v1 as secretManager } from "@google-cloud/secret-manager";
 import { SecretManagerService } from './secret-manager.service';
-import { CLIENT_INSTANCE, SECRETS_PARENT } from './secret-manager.constants';
-import { RootOption } from './types';
+import { CLIENT_INSTANCE, MODULE_OPTIONS, SECRETS_PARENT } from './secret-manager.constants';
+import { ModuleOptions } from './types';
 
 @Global()
-@Module({
-    providers: [SecretManagerService],
-    exports: [SecretManagerService],
-})
+@Module({})
 export class SecretManagerModule {
-    static forRoot(options: RootOption): DynamicModule {
-        const client = new secretManager.SecretManagerServiceClient(options);
+    static forRoot(options: ModuleOptions): DynamicModule {
+        return SecretManagerModule.forRootAsync({ useValue: options });
+    }
+    static forRootAsync(asyncOptions: ModuleAsyncOptions<ModuleOptions>): DynamicModule {
         return {
             module: SecretManagerModule,
+            global: true,
             providers: [
                 {
-                    provide: CLIENT_INSTANCE,
-                    useValue: client,
+                    provide: MODULE_OPTIONS,
+                    ...asyncOptions,
                 },
                 {
+                    inject: [MODULE_OPTIONS],
+                    provide: CLIENT_INSTANCE,
+                    useFactory: (moduleOptions: ModuleOptions) => new secretManager.SecretManagerServiceClient(moduleOptions),
+                },
+                {
+                    inject: [MODULE_OPTIONS],
                     provide: SECRETS_PARENT,
-                    useValue: options.parent
-                }
-            ]
+                    useFactory: ({ parent }: ModuleOptions) => parent,
+                },
+                SecretManagerService
+            ],
+            exports: [SecretManagerService]
         }
     }
 }
--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,8 +1,16 @@
+import { ClassProvider, ValueProvider, FactoryProvider, ExistingProvider } from '@nestjs/common/interfaces';
 import type { ClientOptions } from 'google-gax/build/src/clientInterface';
 import { v1 as secretManager } from "@google-cloud/secret-manager";
+export type SecretManagerClient = secretManager.SecretManagerServiceClient;
 interface DefaultOptions {
     /** parent of secrets */
     parent: number;
 }
-export type RootOption = DefaultOptions & ClientOptions;
-export type SecretManagerClient = secretManager.SecretManagerServiceClient;
+export type ModuleOptions = DefaultOptions & ClientOptions;
+
+export type AsyncProvider<T> =
+  | Omit<ClassProvider<T>, 'provide'>
+  | Omit<ValueProvider<T>, 'provide'>
+  | Omit<FactoryProvider<T>, 'provide'>
+  | Omit<ExistingProvider<T>, 'provide'>;
+export type ModuleAsyncOptions<T> = AsyncProvider<T>;
