f4015a0537236fec491783527698ca7ed82c9e60
bino <bonoself@gmail.com>
2023-04-09 18:17:25 +0900

    feat(ServiceManagerService): put in private secret map

--- lib/secret-manager.service.ts
+++ lib/secret-manager.service.ts
@@ -2,15 +2,15 @@ import { SecretManagerClient } from './types/index';
 import { Inject } from '@nestjs/common/decorators';
 import { CLIENT_INSTANCE, SECRETS_PARENT } from './secret-manager.constants';
 import { Injectable } from '@nestjs/common';
-const _secrets: Map<string, string> = new Map();
 @Injectable()
 export class SecretManagerService {
+    private readonly _secrets: Map<string, string> = new Map()
     constructor(
         @Inject(CLIENT_INSTANCE) private readonly client: SecretManagerClient,
         @Inject(SECRETS_PARENT) private readonly parent: string
     ) {}
     async getSecret(secretName: string, version: 'latest' | number = 'latest') {
-        const secretCache = _secrets.get(secretName);
+        const secretCache = this._secrets.get(secretName);
         if(secretCache) {
             return secretCache;
         }
@@ -19,7 +19,7 @@ export class SecretManagerService {
         
         const secretValue = secret.payload?.data?.toString();
         if(secretValue) {
-            _secrets.set(secretName, secretValue);
+            this._secrets.set(secretName, secretValue);
         }
         return secretValue;
     }
